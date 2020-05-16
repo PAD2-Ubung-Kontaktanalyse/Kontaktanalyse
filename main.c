029f648556fb04121f554a1339c2ffd5d8c86a92
Mathias1991 <65241872+Mathias1991@users.noreply.github.com>
2020-05-16 16:08:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-  
+  //[1] - Verbindung zwischen 2 Personen analysieren 
+  [2] - Alle direkten Kontakte einer Person anzeigen 
+  [3] - Durchschnittskontaktzeit einer Person anzeigen [4] - Statistik anzeigen 
+  [5] - Liste der Personen ausgeben 
+  [6] - Liste der Kontakte ausgeben 
+  [7] - Person erstellen/bearbeiten/entfernen 
+  [8] - Kontakt erstellen/bearbeiten/entfernen 
+  [0] - Beenden
   return 0;
 }

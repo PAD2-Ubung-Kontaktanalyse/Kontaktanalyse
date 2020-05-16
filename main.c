9d229eb420857f8455c80b048abb89ebd453c497
Mathias1991 <65241872+Mathias1991@users.noreply.github.com>
2020-05-16 17:25:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+{
+    int n = 0;
+    
+    printf("Bitte waehlen Sie eine der folgenden Aktionen aus:\n");
+    scanf("%d", &n);
+    printf("             Menu:\n\n");
+    printf("    || [1] - Verbindung zwischen zwei Personen analysieren          ||\n");
+    printf("    || [2]-  Direkte Kontakte zu einer Person anzeigen              ||\n");
+    printf("    || [3] - Durchschnittskontaktzeit einer Person                  ||\n");
+    printf("    || [4]-  Statistik anzeigen                                     ||\n");
+    printf("    || [5] - Liste der Personen ausgeben                            ||\n");
+    printf("    || [6] - Liste der Kontakte ausgeben                            ||\n");
+    printf("    || [7] - Person erstellen/bearbeiten/entfernen                  ||\n");
+    printf("    || [8] - Kontakt erstellen/bearbeiten/entfernen                 ||\n");
+    printf("    || [0] - Programm beenden                                       ||\n");
+
+    scanf("%d", &n);
+
+    switch(n) {
+	case 1: printf("Verbindung zwischen zwei Personen analysieren\n");
+	break;
+	case 2: printf("Direkte Kontakte zu einer Person anzeigen\n");
+	break;
+    case 3: printf("Durchschnittskontaktzeit einer Person\n");
+	break;
+	case 4: printf("Statistik anzeigen\n");
+	break;
+	case 5: printf("Liste der Personen ausgeben\n");
+	break;
+    case 6: printf("Liste der Kontakte ausgeben\n");
+	break;
+	case 7: printf("Person erstellen/bearbeiten/entfernen");
+	break;
+	case 8: printf("Kontakt erstellen/bearbeiten/entfernen");
+	break;
+	case 0: printf("Programm beenden");
+	break;
+	default: printf("Falsche Eingabe!\n");
+}
+
+
+    return 0;
+}
+
+
+
+
+
 int main() {
   //[1] - Verbindung zwischen 2 Personen analysieren 
   [2] - Alle direkten Kontakte einer Person anzeigen 

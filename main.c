0cc381689eeab1454ae193af7aa2e5b1a53c66c1
chryssiegl <64417141+chryssiegl@users.noreply.github.com>
2020-06-12 02:06:22 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "generator/generator.h"
+#include "Graphviz/generate_txt_file.h"
+#include "analysis/analysis.h"
 
+
+char *name_file = "names.csv";
+char *person_file = "";
+char *interaction_file = "";
 
 int main()
 {
@@ -13,146 +20,173 @@ int main()
     int n2 = 0;
     int n3 = 0;
 
+    while(1){ //endless loop for menu
+
+      do{
+          if(n <= 0)
+          {
+              printf("Falsche Eingabe!\n\n");
+          }
+
+          printf("             Bitte waehlen Sie eine der folgenden Aktionen aus:\n\n");
+          printf("             Menu:\n\n");
+          printf("    || [1] - Datensatz generieren                                    ||\n");
+          printf("    || [2]-  Datensatz laden                                    ||\n");
+          printf("    || [3] - Verbindung zwischen zwei Personen analysieren          ||\n");
+          printf("    || [4]-  Liste der Personen ausgeben                            ||\n");
+          printf("    || [5] - Liste der Kontakte ausgeben                            ||\n");
+          printf("    || [6] - Direkte Kontakte zu einer Person anzeigen              ||\n");
+          printf("    || [7] - Durchschnittskontaktzeit einer Person anzeigen         ||\n");
+          printf("    || [8] - Statistik anzeigen                                     ||\n");
+          printf("    || [9] - Programm beenden                                       ||\n");
 
 
-    do{
-        if(n <= 0)
-        {
-            printf("Falsche Eingabe!\n\n");
-        }
+          scanf(" %s", input);
+          n = atoi(input);
 
-        printf("             Bitte waehlen Sie eine der folgenden Aktionen aus:\n\n");
-        printf("             Menu:\n\n");
-        printf("    || [1] - Person erstellen/bearbeiten/entfernen                  ||\n");
-        printf("    || [2]-  Kontakt erstellen/bearbeiten/entfernen                 ||\n");
-        printf("    || [3] - Verbindung zwischen zwei Personen analysieren          ||\n");
-        printf("    || [4]-  Liste der Personen ausgeben                            ||\n");
-        printf("    || [5] - Liste der Kontakte ausgeben                            ||\n");
-        printf("    || [6] - Direkte Kontakte zu einer Person anzeigen              ||\n");
-        printf("    || [7] - Durchschnittskontaktzeit einer Person anzeigen         ||\n");
-        printf("    || [8] - Statistik anzeigen                                     ||\n");
-        printf("    || [9] - Programm beenden                                       ||\n");
+      }while(n <= 0 || n > 9);
 
 
-        scanf(" %s", input);
-        n = atoi(input);
+          if(n >= 1 && n <= 9)
+          {
+              switch(n)
+                  {
 
-    }while(n <= 0 || n > 9);
+                  case 1: do{
+                          printf("Bitte waehlen Sie eine der folgenden Aktionen aus:\n\n");
+                          printf("[0] - Zurück zum Hauptmenue\n");
+                          printf("[1] - Liste mit Personen generieren\n");
+                          printf("[2] - Liste mit Interaktionen generieren\n");
+                          scanf(" %s", input_2);
+                          n2 = atoi(input_2);
+                  }while(n2 < 0 || n2 > 2);
+                          switch (n2) {
+                            case 0: //ZURUECK
+                              break;
 
+                            case 1: //PERSONEN GENERIEREN
+                              printf("\nName des neuen Personen-Datensatzes: ");
+                              scanf(" %s\n", person_file);
 
-        if(n >= 1 && n <= 9)
-        {
-            switch(n)
-                {
+                              int n = 0;
+                              do{
+                                printf("Anzahl der Personen: ");
+                                scanf(" %d", &n);
+                              }while(n <= 0);
 
-                case 1: do{
-                        printf("Bitte waehlen Sie eine der folgenden Aktionen aus:\n\n");
-                        printf("[1] - Person erstellen\n");
-                        printf("[2] - Person bearbeiten\n");
-                        printf("[3] - Person entfernen\n");
-                        scanf(" %s", input_2);
-                        n2 = atoi(input_2);
-                }while(n2 <= 0 || n2 > 3);
-                        break;
+                              float infection_rate = 0;
+                              do{
+                                printf("Infektionsrate (0.0 - 1.0): ");
+                                scanf(" %f", &infection_rate);
+                              }while(n <= 0);
 
-                case 2: do{
-                        printf("Bitte waehlen Sie eine der folgenden Aktionen aus:\n\n");
-                        printf("[1] - Kontakt erstellen\n");
-                        printf("[2] - Kontakt bearbeiten\n");
-                        printf("[3] - Kontakt entfernen\n");
-                        scanf(" %s", input_3);
-                        n3 = atoi(input_3);
-                }while(n3 <= 0 || n3 > 3);
-                        break;
+                              if(generate_persons(name_file, person_file, n, infection_rate) != 0){
+                                printf("Fehler beim Dateizugriff!\n\n");
+                              }
+                              break;
 
-                case 3: printf("Verbindung zwischen zwei Personen analysieren\n");
-                        break;
+                            case 2: //INTERAKTIONEN GENERIEREN
+                              printf("\nName des neuen Interaktions-Datensatzes: ");
+                              scanf(" %s\n", interaction_file);
 
-                case 4: printf("Liste der Personen ausgeben\n");
-                        break;
+                              int n = 0;
+                              do{
+                                printf("Anzahl der Interaktionen: ");
+                                scanf(" %d", &n);
+                              }while(n <= 0);
 
-                case 5: printf("Liste der Kontakte ausgeben\n");
-                        break;
+                              int start_time = 0;
+                              do{
+                                printf("Start-Zeitpunkt (in Minuten seit Epoch): ");
+                                scanf(" %f", &start_time);
+                              }while(n < 0);
 
-                case 6: printf("Direkte Kontakte zu einer Person anzeigen\n");
-                        break;
+                              int end_time = 0;
+                              do{
+                                printf("End-Zeitpunkt (in Minuten seit Epoch): ");
+                                scanf(" %f", &end_time);
+                              }while(n < 0 || (start_time >= end_time));
 
-                case 7: printf("Durchschnittskontaktzeit einer Person anzeigen\n");
-                        break;
+                              int min_time = 0;
+                              do{
+                                printf("Minimale Kontaktzeit (in Minuten): ");
+                                scanf(" %f", &min_time);
+                              }while(n <= 0);
 
-                case 8: printf("Statistik anzeigen\n");
-                        break;
+                              int max_time = 0;
+                              do{
+                                printf("Maximale Kontaktzeit (in Minuten): ");
+                                scanf(" %f", &max_time);
+                              }while(n < 0 || (max_time < min_time));
 
-                case 9: printf("Programm beenden\n");
-                        break;
+                              if(generate_interactions(person_file, interaction_file, n, start_time, end_time, min_time, max_time) != 0){
+                                printf("Fehler beim Dateizugriff!\n\n");
+                              }
+                            break;
+                          }
+                          break;
 
-                default: printf("Falsche Eingabe!\n");
-                }
-        }
-    else
-    {
-        printf("Falsche Eingabe!\n\n");
+                  case 2: do{
+                          printf("Bitte waehlen Sie eine der folgenden Aktionen aus:\n\n");
+                          printf("[0] - Zurück zum Hauptmenue\n");
+                          printf("[1] - Datensatz mit Personen laden\n");
+                          printf("[2] - Datensatz mit Kontakten laden\n");
+                          printf("[3] - Namensliste laden\n")
+                          scanf(" %s", input_3);
+                          n3 = atoi(input_3);
+                  }while(n3 < 0 || n3 > 3);
+                          switch(n3){
+                            case 0: //ZURUECK
+                              break;
+                            case 1:
+                              printf("Pfad und Name des einzulesenden Personen-Datensatzes: ");
+                              scanf(" %s", person_file);
+                              printf("\n");
+                              break;
+                            case 2:
+                              printf("Pfad und Name des einzulesenden Interaktions-Datensatzes: ");
+                              scanf(" %s", interaction_file);
+                              printf("\n");
+                              break;
+                            case 3:
+                              printf("\nPfad und Name der Namensliste (z.B. names.csv): ");
+                              scanf(" %s\n", name_file);
+                              printf("\n");
+                          }
+
+                          break;
+
+                  case 3: printf("Verbindung zwischen zwei Personen analysieren\n");
+                          break;
+
+                  case 4: printf("Liste der Personen ausgeben\n");
+
+                          break;
+
+                  case 5: printf("Liste der Kontakte ausgeben\n");
+                          break;
+
+                  case 6: printf("Direkte Kontakte zu einer Person anzeigen\n");
+                          break;
+
+                  case 7: printf("Durchschnittskontaktzeit einer Person anzeigen\n");
+                          break;
+
+                  case 8: printf("Statistik anzeigen\n");
+                          break;
+
+                  case 9: printf("Programm beenden\n");
+                          exit(EXIT_SUCCESS);
+                          break;
+
+                  default: printf("Falsche Eingabe!\n");
+                  }
+          }
+      else
+      {
+          printf("Falsche Eingabe!\n\n");
+      }
+
     }
-
-
     return 0;
 }
-
-
-/*
-int main() {
-  //[1] - Verbindung zwischen 2 Personen analysieren 
-  [2] - Alle direkten Kontakte einer Person anzeigen 
-  [3] - Durchschnittskontaktzeit einer Person anzeigen [4] - Statistik anzeigen 
-  [5] - Liste der Personen ausgeben 
-  [6] - Liste der Kontakte ausgeben 
-  [7] - Person erstellen/bearbeiten/entfernen 
-  [8] - Kontakt erstellen/bearbeiten/entfernen 
-  [0] - Beenden
-  return 0;
-}
-int n;
-    printf("Menu:\n");
-    printf("[0] - End Program\n");
-    printf("[1] - Verbindung zwischen zwei Personen analysieren.\n");
-    printf("[2]- Direkte Kontakte zu einer Person anzeigen\n");
-    printf("[3] - Durchschnittskontaktzeit einer Person\n");
-    printf("[4]- Statistik anzeigen\n");
-    printf("[5] - Liste der Personen ausgeben\n");
-    printf("[6] - Liste der Kontakte ausgeben\n");
-    printf("[7] - Person erstellen/bearbeiten/entfernen\n");
-    printf("[8] - Kontakt erstellen/bearbeiten/entfernen\n");
-    
-    do {
-    printf("Bitte geben Sie ihre Auswahl an: ");
-    scanf("%d",&n);
-switch (n) {
-  
-      case 1: Verbindung zwischen zwei personen
-      Funtkion_zweiPersonen(){
-      char name1;
-      char name2;
-      fgets(name1);
-      fgets(name2);
-      erstelle Liste mit kontakten
-      print Liste
-      }
-      case 2: Alle direkten Kontakte einer Person
-      case 3: Durchschnittskontaktzeit einer Person
-      case 4: Statistik anzeigen
-      case 5: Liste der Personen ausgeben
-       print liste 
-      case 6: Liste der Kontakte ausgeben
-      case 7: Person erstellen/bearbeiten/entfernen
-            case1: erstellen
-            case2: bearbeiten
-            case3: entfernen 
-      case 8: Kontakt erstellen/bearbeiten/entfernen
-          case1: erstellen
-          case2: bearbeiten
-          case3: entfernen
-      case 9: beenden
-        allen speicher freigeben
-        freundlich ciao sagen
-        beenden 
-}*/

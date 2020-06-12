8264b507ce75fd8c530204b2699cf4400f55bdb8
chryssiegl <64417141+chryssiegl@users.noreply.github.com>
2020-06-12 18:29:22 +0200

    Update main.c

--- demo/main.c
+++ demo/main.c
@@ -5,6 +5,53 @@
 
 #define NAME_LEN 61
 
+
+/**
+ *  @brief Reads generated List and converts it into Graphviz script
+ *  @details Format of output file: .txt with script
+ *  @param persons_path Path + file name of the full list of persons
+ *  @param interactions_path Path + file name of the full list of interactions
+ *  @param output_path Path + file name where to output should be saved
+ *  @param start_timeframe Beginning of the time frame in minutes since epoch, -1 to disable timeframe
+ *  @param end_timeframe End of the time frame in minutes since epoch
+ *  @return Returns 0 if successful, -1 in case of an error
+ */
+ 
+int create_graph(char *persons_path, char *interactions_path, char *output_path, int start_timeframe, int end_timeframe);
+
+/**
+ *  @brief Reads list of names from a text file, randomly generates persons and family stuctures, and saves the output in a text file
+ *  @details Format of output file: person-id | name | family-id | infection-status
+ *  @param input_path Path + file name of the full list of names
+ *  @param output_path Path + file name where to output should be saved
+ *  @param n Number of persons to generate
+ *  @param infection_rate Probability for a person to be flagged "infected". Range: [0-1]
+ *  @return Returns 0 in case of success, -1 for input error, -2 for output error
+ */
+int generate_persons(char * input_path, char * output_path, int n, float infection_rate);
+
+/**
+ *  @brief Reads list of persons from a text file, randomly generates interactions, and saves the output in a text file
+ *  @details Format of output file: interaction-id | person1-id | person2-id | start-time | end-time
+ *  @param input_path Path + file name of the full list of names
+ *  @param output_path Path + file name where to output should be saved
+ *  @param n Maximum number of interactions to generate (0 for unlimited)
+ *  @param start_time Start of the time frame for all interactions in minutes since epoch
+ *  @param end_time End of the time frame for all interactions in minutes since epoch
+ *  @param min_time Minimum time span of an interaction in minutes
+ *  @param max_time Maximum time span of an interaction in minutes
+ *  @return Returns 0 in case of sucess, -1 for input error, -2 for output error
+ */
+int generate_interactions(char * input_path, char * output_path, int n, int start_time, int end_time, int min_time, int max_time);
+
+/**
+ *  @brief Counts the number of lines of a file
+ *  @param input_file Pointer to input file stream
+ *  @return Number of lines
+ */
+int line_count(FILE * input_file);
+
+
 int generate_persons(char * input_path, char * output_path, int n, float infection_rate){
     int name_count = 0;
     

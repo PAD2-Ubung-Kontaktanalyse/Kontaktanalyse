64f2d70b687d6647d8aa46ec1aca5aadb74d6e4d
meisisoiisme <64416712+meisisoiisme@users.noreply.github.com>
2020-06-12 14:38:23 +0200

    Create create_list_interactions.h

---
+++ Graphviz/create_list_interactions.h
@@ -0,0 +1,22 @@
+//
+//  Funktion zum erzeugen von Listen 
+//  die Interaktionen einer Person ausgibt
+//
+
+#ifndef generate_list_interactions_h
+#define generate_list_interactions_h
+
+/**
+ *  @brief Reads generated List and converts it into new List
+ *  @details Format of output file: .txt 
+ *  @param persons_path Path + file name of the full list of persons
+ *  @param interactions_path Path + file name of the full list of interactions
+ *  @param output_path Path + file name where to output should be saved
+ *  @param start_timeframe Beginning of the time frame in minutes since epoch, -1 to disable timeframe
+ *  @param end_timeframe End of the time frame in minutes since epoch
+ *  @return Returns 0 if successful, -1 in case of an error
+ */
+ 
+int create_list_interactions(char *persons_path, char *interactions_path, char *output_path, int start_timeframe, int end_timeframe)
+
+#endif /* generate_list_interactions_h */

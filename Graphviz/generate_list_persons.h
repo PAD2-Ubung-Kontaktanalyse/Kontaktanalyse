c0fce4b42acafd7c218ed77426cd4af914f35034
meisisoiisme <64416712+meisisoiisme@users.noreply.github.com>
2020-06-12 14:43:07 +0200

    Create generate_list_persons.h

---
+++ Graphviz/generate_list_persons.h
@@ -0,0 +1,21 @@
+//
+//  Funktion zum erzeugen von Listen 
+//  einer Liste aller Personen
+//
+
+#ifndef generate_txt_file_h
+#define generate_txt_file_h
+
+/**
+ *  @brief Reads generated List and creates new List
+ *  @details Format of output file: .txt 
+ *  @param persons_path Path + file name of the full list of persons
+ *  @param output_path Path + file name where to output should be saved
+ *  @param Name_Person Name of a Person
+ *  @return Returns 0 if successful, -1 in case of an error
+ */
+ 
+int create_list_persons(char *persons_path, char *output_path, char *Name_Person){
+
+#endif /* generate_txt_file_h */
+
